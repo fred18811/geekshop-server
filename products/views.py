72ca6a0a62d38086eacbb3b31a8c5890990f3b85
simakin <fred17@mail.ru>
2021-06-28 13:29:04 +0300

    added filte capfirst, title and date in footer on products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 
 
@@ -16,8 +18,9 @@ def products(request):
             {'name':'Синяя куртка The North Face','price':23725,'description':'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.','img':'Blue-jacket-The-North-Face.png'},
             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price':3390, 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.','img':'Brown-sports-oversized-top-ASOS-DESIGN.png'},
             {'name': 'Черный рюкзак Nike Heritage', 'price':2340, 'description': 'Плотная ткань. Легкий материал.','img':'Black-Nike-Heritage-backpack.png'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price':13590, 'description': 'Гладкий кожаный верх. Натуральный материал.','img':'Black-Dr-Martens-shoes.png'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price':2890, 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.','img':'Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
-        ]
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price':13590, 'description': 'гладкий кожаный верх. Натуральный материал.','img':'Black-Dr-Martens-shoes.png'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price':2890, 'description': 'легкая эластичная ткань сирсакер Фактурная ткань.','img':'Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
+        ],
+        'date': datetime.datetime.now(),
     }
     return render(request, 'products/products.html', context)
