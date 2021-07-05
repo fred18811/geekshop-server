f2cf9c3418de5c4057c56b7f66810093a17e641b
simakin <fred17@mail.ru>
2021-07-05 11:22:41 +0300

    added user authorizations

--- users/views.py
+++ users/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
+
 
 def registration(request):
     return render(request, 'users/registration.html')
