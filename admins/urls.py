a247fdda1e4dbf1142f5128b49edac3b881b0e97
simakin <fred17@mail.ru>
2021-07-14 13:49:01 +0300

    added CRUD for admins app

--- admins/urls.py
+++ admins/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from admins.views import index, admin_users_create, admin_users_update_delete, admin_users
+from admins.views import index, admin_users_create, admin_users_update, admin_users, admin_users_remove
 
 app_name = 'admins'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/', admin_users, name='admin_users'),
     path('users/create', admin_users_create, name='admin_users_create'),
-    path('users/update', admin_users_update_delete, name='admin_users_update_delete'),
+    path('users/update/<int:pk>', admin_users_update, name='admin_users_update'),
+    path('users/remove/<int:pk>', admin_users_remove, name='admin_users_remove'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from users.models import User
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
 
 def index(request):
     context = {
@@ -6,20 +11,48 @@ def index(request):
     }
     return render(request, 'admins/index.html', context)
 
+
 def admin_users(request):
     context = {
         'title': 'GeekShop - Пользователи',
+        'users': User.objects.all(),
     }
     return render(request, 'admins/admin-users-read.html', context)
 
+
 def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
     context = {
         'title': 'GeekShop - Создание пользователя',
+        'form': form,
     }
     return render(request, 'admins/admin-users-create.html', context)
 
-def admin_users_update_delete(request):
+
+def admin_users_update(request, pk):
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
     context = {
         'title': 'GeekShop - Обновление\удаление пользователя',
+        'form': form,
+        'selected_user':selected_user
     }
-    return render(request, 'admins/admin-users-update_delete.html', context)
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+def admin_users_remove(request, pk):
+    user = User.objects.get(id=pk)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
