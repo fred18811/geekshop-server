e011ec11a3bd0b35ec1a2e4f6feb4d6612e24db4
simakin <fred17@mail.ru>
2021-08-10 12:32:24 +0300

    Expanded the functionality of working with forms using "django-dynamic-formset"

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -53,4 +53,18 @@ window.addEventListener('load',()=>{
 		$('.order_total_cost').html(order_total_cost);
 		order_total_cost = Number(order_total_cost);
 	}
+	
+	$('.formset_row').formset({
+		addText: 'добавить товар',
+		deleteText: 'удалить',
+		prefix: 'orderitems',
+		removed: deleteOrderItem
+	});
+	
+	function deleteOrderItem(row){
+		let target_name = row[0].querySelector('input[type=number]').name;
+		orderitem_num = target_name.replace('orderitems-','').replace('-quantity','');
+		delta_quantity = -quantity_arr[orderitem_num];
+		orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+	}
 })
